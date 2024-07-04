fc6a63a7f66b67173e4d3ed6c8c2cdc0df962e0b
badrivarun02 <badrivarun02@gmail.com>
2024-07-04 19:19:59 +0530

    ADDED test files, pom file updated, .dockefiles

---
+++ src/test/java/com/rockstar/StartApplicationTest.java
@@ -0,0 +1,30 @@
+package com.rockstar;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.ui.Model;
+
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class StartApplicationTest {
+
+  @Mock
+  private StartApplication startApplication;
+
+  @Test
+  public void testIndex_ReturnsCorrectViewName() {
+    // Mock the behavior of startApplication.index
+    when(startApplication.index(null)).thenReturn("index");
+
+    String viewName = startApplication.index(null);
+    assertEquals("index", viewName);
+  }
+}
+
+
