053a64aad9f50de61a0bd68c11dfd73ab3756006
badrivarun02 <112253730+badrivarun02@users.noreply.github.com>
2024-07-13 20:53:33 +0530

    Updated StartApplicationTest.java

--- src/test/java/com/rockstar/StartApplicationTest.java
+++ src/test/java/com/rockstar/StartApplicationTest.java
@@ -1,30 +1,24 @@
 package com.rockstar;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.ui.Model;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class StartApplicationTest {
 
-  @Mock
-  private StartApplication startApplication;
+    @Mock
+    private StartApplication startApplication;
 
-  @Test
-  void testIndex_ReturnsCorrectViewName() {
-    // Mock the behavior of startApplication.index
-    when(startApplication.index(null)).thenReturn("index");
+    @Test
+    void givenNullModel_whenIndexCalled_thenReturnIndexViewName() {
+        // Mock the behavior of startApplication.index
+        when(startApplication.index(null)).thenReturn("index");
 
-    String viewName = startApplication.index(null);
-    assertEquals("index", viewName);
-  }
+        String viewName = startApplication.index(null);
+        assertThat(viewName).isEqualTo("index");
+    }
 }
-
-
